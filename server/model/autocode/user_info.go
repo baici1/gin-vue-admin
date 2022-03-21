934e73fb3043aa0d3571a02f2287598b77f3b714
baici1 <249337001@qq.com>
2022-03-21 21:21:51 +0800

    curd userInfo表

---
+++ server/model/autocode/user_info.go
@@ -0,0 +1,22 @@
+// 自动生成模板UserInfo
+package autocode
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+)
+
+// UserInfo 结构体
+// 如果含有time.Time 请自行import time包
+type UserInfo struct {
+	global.GVA_MODEL
+	Password string `json:"password" form:"password" gorm:"column:password;comment:密码;size:255;"`
+	Phone    string `json:"phone" form:"phone" gorm:"column:phone;comment:手机号;"`
+	Slat     string `json:"slat" form:"slat" gorm:"column:slat;comment:加盐;size:255;"`
+	Identity *int   `json:"identity" form:"identity" gorm:"column:identity;comment:身份 1是老师 0是学生;size:10;"`
+	Check    *int   `json:"check" form:"check" gorm:"column:check;comment:审核老师;size:10;"`
+}
+
+// TableName UserInfo 表名
+func (UserInfo) TableName() string {
+	return "user_info"
+}
---
+++ server/service/autocode/user_info.go
@@ -0,0 +1,66 @@
+package autocode
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/global"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/autocode"
+	autoCodeReq "github.com/flipped-aurora/gin-vue-admin/server/model/autocode/request"
+	"github.com/flipped-aurora/gin-vue-admin/server/model/common/request"
+)
+
+type UserInfoService struct {
+}
+
+// CreateUserInfo 创建UserInfo记录
+// Author [piexlmax](https://github.com/piexlmax)
+func (userInfoService *UserInfoService) CreateUserInfo(userInfo autocode.UserInfo) (err error) {
+	err = global.GVA_DB.Create(&userInfo).Error
+	return err
+}
+
+// DeleteUserInfo 删除UserInfo记录
+// Author [piexlmax](https://github.com/piexlmax)
+func (userInfoService *UserInfoService) DeleteUserInfo(userInfo autocode.UserInfo) (err error) {
+	err = global.GVA_DB.Delete(&userInfo).Error
+	return err
+}
+
+// DeleteUserInfoByIds 批量删除UserInfo记录
+// Author [piexlmax](https://github.com/piexlmax)
+func (userInfoService *UserInfoService) DeleteUserInfoByIds(ids request.IdsReq) (err error) {
+	err = global.GVA_DB.Delete(&[]autocode.UserInfo{}, "id in ?", ids.Ids).Error
+	return err
+}
+
+// UpdateUserInfo 更新UserInfo记录
+// Author [piexlmax](https://github.com/piexlmax)
+func (userInfoService *UserInfoService) UpdateUserInfo(userInfo autocode.UserInfo) (err error) {
+	err = global.GVA_DB.Save(&userInfo).Error
+	return err
+}
+
+// GetUserInfo 根据id获取UserInfo记录
+// Author [piexlmax](https://github.com/piexlmax)
+func (userInfoService *UserInfoService) GetUserInfo(id uint) (err error, userInfo autocode.UserInfo) {
+	err = global.GVA_DB.Where("id = ?", id).First(&userInfo).Error
+	return
+}
+
+// GetUserInfoInfoList 分页获取UserInfo记录
+// Author [piexlmax](https://github.com/piexlmax)
+func (userInfoService *UserInfoService) GetUserInfoInfoList(info autoCodeReq.UserInfoSearch) (err error, list interface{}, total int64) {
+	limit := info.PageSize
+	offset := info.PageSize * (info.Page - 1)
+	// 创建db
+	db := global.GVA_DB.Model(&autocode.UserInfo{})
+	var userInfos []autocode.UserInfo
+	// 如果有条件搜索 下方会自动创建搜索语句
+	if info.Phone != "" {
+		db = db.Where("phone = ?", info.Phone)
+	}
+	err = db.Count(&total).Error
+	if err != nil {
+		return
+	}
+	err = db.Limit(limit).Offset(offset).Find(&userInfos).Error
+	return err, userInfos, total
+}
